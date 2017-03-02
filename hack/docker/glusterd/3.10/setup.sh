54182b82303e7f39e5170b1ece59c51eb7a0b709
Tamal Saha <TamalSaha@users.noreply.github.com>
2017-03-02 00:58:05 -0800

    Add GlusterFS 3.10 image (#29)
    ref: https://blog.gluster.org/2017/02/announcing-gluster-3-10/

---
+++ hack/docker/glusterd/3.10/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+REPO_ROOT=$(go env GOPATH)/src/github.com/appscode/glusterfs
+source "$REPO_ROOT/hack/libbuild/common/lib.sh"
+source "$REPO_ROOT/hack/libbuild/common/public_image.sh"
+
+IMG=glusterd
+TAG=3.10
+PRIVILEGED_CONTAINER='--privileged=true'
+
+binary_repo $@
